c0f4574bc6715cc95b639e748d7a0c54c46df9fb
Andrew Mohrland <amohrland@gmail.com>
2012-09-01 16:31:52 -0700

    fix bug in get-energies.sh

--- get-energies.sh
+++ get-energies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-mkdir ./data &&
-grep -r  'free  energy' . | sort | awk '{gsub(/\.\//,"",$1); gsub(/\/.*/,"",$1); print $1 " " $6}' > ./data/energies.dat &&
-pushd ./data &&
-git init &&
-git add . &&
-git commit --allow-empty-message -m '' &&
+mkdir ./data ;
+grep -r  'free  energy' . | sort | awk '{gsub(/\.\//,"",$1); gsub(/\/.*/,"",$1); print $1 " " $6}' > ./data/energies.dat ;
+pushd ./data ;
+git init ;
+git add . ;
+git commit --allow-empty-message -m '' ;
 popd
