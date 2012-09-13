0854de43fb5cead78fc5a29df8a01b5ed53e689c
Andrew Mohrland <amohrland@gmail.com>
2012-09-13 16:55:57 -0700

    vtools collects data from \'energies.dat\', so we mv old data to another name, and pipe new data into energies.dat

--- get-energies.sh
+++ get-energies.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# backup old data file
 date=`date +"%Y-%m-%d-%H-%M-%S"`
-mkdir ./data ;
+mkdir ./data/
+mkdir ./data/old/
+mv ./data/energies.dat ./data/old/energies-$date.dat
 
+# create new data file
 for dir in $( ls ); do
     if (tail $dir/OUTCAR | grep "Voluntary"); then 
-	grep 'free  energy' $dir/OUTCAR | tail -1 | awk -v dir=${dir} '{print dir " " $5}' >> ./data/energies-$date.dat ;
+	grep 'free  energy' $dir/OUTCAR | tail -1 | awk -v dir=${dir} '{print dir " " $5}' >> ./data/energies.dat ;
     fi
 done
 
